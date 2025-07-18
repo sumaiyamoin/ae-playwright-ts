168e4ec5026032d550cd134df5e1374a37e5bee0
sumaiyamoin <sumaiyamoin@gmaul.com>
2025-07-18 18:17:08 +0600

    removed placeOrder page

--- pages/NavbarComponent.ts
+++ pages/NavbarComponent.ts
@@ -69,6 +69,17 @@ class NavbarComponent {
         //await expect(this.page.getByRole('heading',{name:'Login to your account'})).toBeVisible();
         const afterLogoutText = this.page.getByRole('heading',{name:'Login to your account'});
     }
+    
+    async placeOrder(itemName: string) {
+        // await this.page.locator(`text=${itemName}`).nth(0).hover();
+        await this.page.waitForSelector('text=Blue Top', { state: 'visible' });
+        await this.page.locator(`.single-products:has-text("${itemName}")`).first().hover();
+        await this.page.locator('.overlay-content > .add-to-cart').first().click();
+       // await page.locator('.overlay-content > .btn').first().click();
+        await this.page.getByText('View Cart').click();        
+        await this.page.getByText('Proceed To Checkout').click();
+        await this.page.getByRole('link', { name: 'Register / Login' }).click();
+    }
 }
 
 export {NavbarComponent}
--- tests/placeOrder.spec.ts
+++ tests/placeOrder.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { PlaceOrderPage } from '../pages/PlaceOrderPage';
+//import { PlaceOrderPage } from '../pages/PlaceOrderPage';
 import { SignUpPage, registerData } from '../pages/SignUpPage';
 //import { LoginPage } from '../pages/LoginPage';
 import { faker } from '@faker-js/faker'
@@ -8,7 +8,7 @@ import { HomePage } from '../pages/HomePage';
 import { LoginPage } from '../pages/LoginPage';
 import { NavbarComponent } from '../pages/NavbarComponent';
 
-test.only('User should be able to register during checkout process', async ({ page }) => {
+test('User should be able to register during checkout process', async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.visit();
 
@@ -49,63 +49,18 @@ test.only('User should be able to register during checkout process', async ({ pa
 
 })
 
-/*
-test('User should be able to register during checkout process', async ({ page }) => {
+
+test.only('User should be able to login during checkout process', async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.visit();
 
-    await homePage.addToCart('Blue Top');
-
-    const placeOrderPage=new PlaceOrderPage(page);
-    await placeOrderPage.placeOrder('Blue Top');
-
-    const signUpPage = new SignUpPage(page);
-    const email = faker.internet.email().toLocaleLowerCase();
     
-    const registerData: registerData = {
-        name: 'ABC',
-        email,
-        password: '1234',
-        state: 'England',
-        city: 'London',
-        address: 'Dhaka',
-        firstName: 'Test',
-        lastName: 'User',
-        zipCode: '40',
-        mobileNumber: '01711012345',
-        day: "1",
-        month: "2",
-        year: "2020"
-    };
-
-    await signUpPage.register(registerData);
-
-    const checkOutPage = new CheckoutPage(page);
-
-    const itemName = {
-        nameOnCard: 'test',
-        cardNum: '5555',
-        cvc: '311',
-        expirationMonth: '12',
-        expirationYear: '2025'
-    };
-
-    await checkOutPage.proceedToCheckout(itemName);
-
-
-})
-
-*/
-
-
-test('User should be able to login during checkout process', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.visit();
-
-    const placeOrderPage=new PlaceOrderPage(page);
-    await placeOrderPage.placeOrder('Blue Top');
-
+    //const placeOrderPage=new PlaceOrderPage(page);
+    //await placeOrderPage.placeOrder('Blue Top');
     const navBarComponent = new NavbarComponent(page);
+    await navBarComponent.placeOrder('Blue Top');
+
+    //const navBarComponent = new NavbarComponent(page);
     await navBarComponent.login('Leilani.Mertz@hotmail.com', 'testpassword');
 
 
